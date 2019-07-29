8d8b39cab913eee11eb148ef763a2aa7ba4dc188
Alina Ray <a.alina.ray@gmail.com>
2019-07-29 23:24:34 +0300

    Updates

---
+++ precache-manifest.2f0eddfe172b45149aca27edf2b68448.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f5c5d74352d4a31b7ee53a39aa824f4",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "6b51be3b67085ce6348f",
+    "url": "/react_tabs/static/css/main.83286ba8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react_tabs/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "6b51be3b67085ce6348f",
+    "url": "/react_tabs/static/js/main.ca6f5d86.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
